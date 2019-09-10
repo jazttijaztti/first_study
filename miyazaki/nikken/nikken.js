06523bce85ab581cd0373083a72e22f1e7cc742c
miyazaki <myzky0404@gmail.com>
2019-09-10 19:26:12 +0900

    0909

--- miyazaki/nikken/nikken.js
+++ miyazaki/nikken/nikken.js
@@ -1,11 +1,21 @@
 $(function(){
 
   $('.img1').slideUp(2000);
-  $('.img2').fadeIn(4000);
+  $('.img2').fadeIn(2000);
   $('.img2').slideUp(2000);
-  $('.img3').fadeIn(5000);
+  $('.img3').fadeIn(2000);
 
 
-  // $('.img1').delay(3000).attr('src','4.png');
+
+  $('.js2').on('mouseover', function(){
+    $('.content4-up-right img').attr('src', '16.png');
+    setTimeout(function(){
+    $('.content4-up-right img').attr('src', '20.png');
+    },2000);
+
+
+
+  });
+
 
 });
