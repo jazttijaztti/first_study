614117fd5ba80b7b904ad9175672a566695f5b1d
miyazaki <myzky0404@gmail.com>
2019-09-12 09:56:01 +0900

    0912

--- miyazaki/nikken/nikken.js
+++ miyazaki/nikken/nikken.js
@@ -1,9 +1,31 @@
 $(function(){
-
   $('.img1').slideUp(2000);
   $('.img2').fadeIn(2000);
   $('.img2').slideUp(2000);
   $('.img3').fadeIn(2000);
+  var roop = 1;
+  $(window).scroll(function() {
+
+  console.log("スクロール値は$(this).scrollTop()でとる。とった値は→"+ $(this).scrollTop());
+  var scrlTop = $(this).scrollTop();
+  var h3scrl = $('.content2').offset().top;
+  console.log("content2値は"+ h3scrl);
+  console.log("roopは"+ roop);
+  if (h3scrl < scrlTop && roop == 1) {
+  console.log(h3scrl);
+  roop = roop + 1;
+  console.log("roopは"+ roop);
+  
+  $('#wrapper').animate({
+    'width': '50%',
+  'backgroundColor': "#99ccff"
+  },2000).animate({
+    'width': '100%',
+  'backgroundColor': "white",
+  },2000);
+  }
+ });
+
 
 
 
@@ -12,8 +34,6 @@ $(function(){
     setTimeout(function(){
     $('.content4-up-right img').attr('src', '20.png');
     },2000);
-
-
 
   });
 
